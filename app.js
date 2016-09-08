4d5173400b993ad7533408a2cd087e8f8ad995f4
chadhartman <chad+github@tealium.com>
2016-09-08 15:01:44 -0700

    simplified interface

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 "use strict"
 
-
-
 angular.module('app', []).controller('MainController', ['$scope', function($scope) {
 
     var getRandomInt = function(min, max) {
@@ -21,39 +19,45 @@ angular.module('app', []).controller('MainController', ['$scope', function($scop
     };
 
     $scope.numCards = 6;
-    $scope.characters = {
-        available: [
+
+    $scope.categories = [{
+            name:"characters",
+            available: [
                 "1", "2", "3", "4", "5", "6", 
                 "7", "8", "9", "10", "11", "12", 
                 "13", "14", "15", "16", "17", "18", 
                 "P1", "HS1", "HS2", "HS3", "HS4", "HS5", "HS6"
             ],
-        selected: []
-    };
-    $scope.locations = {
-        available: [
+            selected: []
+        }, {
+            name:"locations",
+            available: [
                 "19", "20", "21", "22", "23", "24", 
                 "25", "26", "27", "28", "29", "30",
                 "31", "32", "33", "34", "35", "36", 
                 "P2", "HS7", "HS8", "HS9", "HS10", "HS11", "HS12"
             ],
-        selected: []
-    };
-    $scope.objects = {
-        available: [
+            selected: []
+        }, {
+            name:"objects",
+            available: [
                 "37", "38", "39", "40", "41", "42", 
                 "43", "44", "45", "46", "47", "48",
                 "49", "50", "51", "52", "53", "54",
                 "P3", "HS13", "HS14", "HS15", "HS16", "HS17", "HS18"
             ],
-        selected: []
+            selected: []
+        }
+    ];
+
+    
+    $scope.addCard = function(increment) {
+        $scope.numCards += increment;
+
+        for(var i = 0; i < $scope.categories.length; i++) {
+            randomlySelect($scope.categories[i]);
+        }
     };
 
-    $scope.onNumCardsChanged = function() {
-        randomlySelect($scope.characters);
-        randomlySelect($scope.locations);
-        randomlySelect($scope.objects);
-    };
-
-    $scope.onNumCardsChanged();
+    $scope.addCard(0);
 }]);

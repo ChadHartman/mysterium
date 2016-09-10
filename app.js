0185543100ef28b2c87f6d4d996c9c8bfd55ec04
chadhartman <chad+github@tealium.com>
2016-09-09 23:57:36 -0700

    Expansions are now filterable

--- app.js
+++ app.js
@@ -17,12 +17,13 @@ angular.module('app', []).controller('MainController', ['$scope', function ($sco
         collection.selected = [];
 
         var selectedIndexes = new Set();
-        var inclusiveMax = collection.available.length - 1;
+        
         while(selectedIndexes.size < $scope.numCards) {
-            selectedIndexes.add(getRandomInt(0, inclusiveMax));
+            selectedIndexes.add(getRandomInt(0, collection.available.length));
         } 
 
         selectedIndexes = Array.from(selectedIndexes);
+        // Simpler to sort the indices than the selected cards
         selectedIndexes = selectedIndexes.sort(intSorter);
 
         for (var i = 0; i < selectedIndexes.length; i++) {
@@ -32,43 +33,63 @@ angular.module('app', []).controller('MainController', ['$scope', function ($sco
     };
 
     $scope.numCards = 6;
+    $scope.hasPromo = true;
+    $scope.hasHiddenSigns = true;
 
+    // TODO: move to Model
     $scope.categories = [{
         name: "characters",
-        available: [
+        available: [],
+        core: [
             "1", "2", "3", "4", "5", "6",
             "7", "8", "9", "10", "11", "12",
-            "13", "14", "15", "16", "17", "18",
-            "P1", "HS1", "HS2", "HS3", "HS4", "HS5", "HS6"
+            "13", "14", "15", "16", "17", "18"
         ],
+        promo: ["P1"],
+        hiddenSigns: ["HS1", "HS2", "HS3", "HS4", "HS5", "HS6"],
         selected: []
     }, {
             name: "locations",
-            available: [
+            available: [],
+            core: [
                 "19", "20", "21", "22", "23", "24",
                 "25", "26", "27", "28", "29", "30",
-                "31", "32", "33", "34", "35", "36",
-                "P2", "HS7", "HS8", "HS9", "HS10", "HS11", "HS12"
+                "31", "32", "33", "34", "35", "36"
             ],
+            promo: ["P2"],
+            hiddenSigns: ["HS7", "HS8", "HS9", "HS10", "HS11", "HS12"],
             selected: []
         }, {
             name: "objects",
-            available: [
+            available: [],
+            core: [
                 "37", "38", "39", "40", "41", "42",
                 "43", "44", "45", "46", "47", "48",
-                "49", "50", "51", "52", "53", "54",
-                "P3", "HS13", "HS14", "HS15", "HS16", "HS17", "HS18"
+                "49", "50", "51", "52", "53", "54"
             ],
+            promo: ["P3"],
+            hiddenSigns: ["HS13", "HS14", "HS15", "HS16", "HS17", "HS18"],
             selected: []
         }
     ];
-
 
     $scope.addCard = function (increment) {
         $scope.numCards += increment;
 
         for (var i = 0; i < $scope.categories.length; i++) {
-            randomlySelect($scope.categories[i]);
+            var category = $scope.categories[i];
+
+            category.available = category.core.slice(0);
+
+            if($scope.hasPromo) {
+                category.available = category.available.concat(category.promo);
+            }
+
+            if($scope.hasHiddenSigns) {
+                category.available = category.available.concat(category.hiddenSigns);
+            }
+
+            randomlySelect(category);
         }
     };
 
